c4ffc9a153fe3a3c3647302efc95f271b13b8c36
Stubborn <131737791+stubbornlove@users.noreply.github.com>
2024-05-27 10:05:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ class Config(object):
     
     API_ID = int(os.environ.get("API_ID", "29768900"))
     API_HASH = os.environ.get("API_HASH", "6a56e5c2383dc7b85210febe60b9f229")
-    
     AUTH_USERS = os.environ.get("AUTH_USERS","5986670447")
     
